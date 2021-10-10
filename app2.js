61fc52131632492098e0995daed51befbc15b947
RILEY MORGAN <MORGARIL0@GMAIL.COM>
2021-10-09 23:39:45 -0700

    added img

--- app2.js
+++ app2.js
@@ -2,8 +2,8 @@ let userAnswer = prompt("Will you save the sharks?");
 console.log('The answer to the call is: ' + userAnswer);
 
 if(userAnswer == 'yes'){
-    let url = "https://toppng.com/uploads/preview/shark-drawing-kawaii-cute-shark-drawi-11563011332xnn4nedjfz.png";
+    let img = "shark-drawing-kawaii-cute-shark-drawi-11563011332xnn4nedjfz.png";
 
 document.write('<img src"' + url + '">');
-//<img src="https://toppng.com/uploads/preview/shark-drawing-kawaii-cute-shark-drawi-11563011332xnn4nedjfz.png">
+//<img src="shark-drawing-kawaii-cute-shark-drawi-11563011332xnn4nedjfz.png">
 }

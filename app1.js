b4aff1887076c2d07ccda7f8d7c7e2f7bc7e1f65
RILEY MORGAN <MORGARIL0@GMAIL.COM>
2021-10-09 20:34:38 -0700

    created js file for webpage

---
+++ app1.js
@@ -0,0 +1,23 @@
+// console.log ("We Made It");
+
+// let userName = 'Rad';
+// let userLastName = 'Riley';
+
+// console.log(userName + ' ' + userLastName)
+
+// console.log(typeof(userName));
+
+// let userTwo = 'Killer';
+// console.log(typeof(userTwo));
+
+// let userAge = 27;
+// console.log(typeof(userAge));
+
+// let isSharkFan = true;
+// console.log(typeof(isSharkFan));
+
+let userName = prompt('Enter the secret word!');
+console.log(userName);
+
+document.write(userName);
+
